07796f5f13471a10f540f153905ffe7f0be8f005
Matthias Bogaert <hello@matthiasbogaert.be>
2018-05-25 17:14:14 +0200

    first

---
+++ todo app/CreateTaskViewController.swift
@@ -0,0 +1,42 @@
+//
+//  CreateTaskViewController.swift
+//  todo app
+//
+//  Created by De Winter Laura on 25/05/18.
+//  Copyright © 2018 Comflow. All rights reserved.
+//
+
+import UIKit
+
+class CreateTaskViewController: UIViewController {
+
+    
+    
+    @IBOutlet weak var importantSwitch: UISwitch!
+    @IBOutlet weak var TaskNameTextField: UITextField!
+    
+    var previousVC = ViewController()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func addTapped(_ sender: Any) {
+        
+        // Create task from outlet information
+        
+        let task = Task()
+        task.name = TaskNameTextField.text!
+        task.important = importantSwitch.isOn
+        
+        
+        // Add new task to array in previous viewcontroller
+        
+        
+        
+        
+    }
+   
+}
